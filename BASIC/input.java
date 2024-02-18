1fdde6e97992589dc5b687bb66aacbe786b5b3a0
Chetan Banjan <chetanbanjan27@gmail.com>
2024-02-18 23:06:26 +0530

    come on

--- BASIC/input.java
+++ BASIC/input.java
@@ -7,11 +7,10 @@ public class input{
         Scanner scn = new Scanner(System.in);
         int n = Integer.parseInt(scn.nextLine());
         String name = scn.nextLine();
-        System.out.println(" Dear " + name + " Here is the counting. ");
-        for(int i=1 ; i<=n ; i++)
-        {
+
+        System.out.println( " Dear" + name + " Here is the counting ");
+        for(int i = 1; i<=n ; i++){
             System.out.println(i);
         }
-
     }
 }
---
+++ BASIC/loops.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class loops{
+    public static void main(String[] args)
+    {
+        int i = 1;
+
+        while( i <=9){
+            System.out.println(i);
+            i++;
+
+        }
+        System.out.println("done");
+    }
+
+    
+}
